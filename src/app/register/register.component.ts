e181a2d0673cc77c89a5651c8f53dfe24b6a8f6b
azharjk <azhr.project@gmail.com>
2023-02-12 02:11:22 +0700

    redirect after registered

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AuthService} from "../auth/auth.service";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -9,7 +10,7 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 })
 export class RegisterComponent {
   form: FormGroup;
-  constructor(private auth: AuthService, private fb: FormBuilder) {
+  constructor(private auth: AuthService, private fb: FormBuilder, private router: Router) {
     this.form = fb.group({
       name: [''],
       email: [''],
@@ -20,7 +21,8 @@ export class RegisterComponent {
 
   submit() {
     this.auth.register(this.form.value).subscribe(() => {
-      console.log("login proced");
+      this.router.navigate([''])
+        .catch(err => console.log(err));
     })
   }
 }
