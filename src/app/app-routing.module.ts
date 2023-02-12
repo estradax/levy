6989ed1de67f52cf5bcf24f92bcddd6e247c81ff
azharjk <azhr.project@gmail.com>
2023-02-12 11:16:16 +0700

    add dashboard page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
   {
     path: 'login',
     component: LoginComponent
@@ -11,7 +16,7 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
-  }
+  },
 ];
 
 @NgModule({
