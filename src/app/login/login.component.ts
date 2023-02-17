094b27bd1014ff13621a013bfa58b4376e87a602
azharjk <azhr.project@gmail.com>
2023-02-17 18:36:59 +0700

    refactor login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
 
@@ -11,20 +11,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  form: FormGroup;
+  loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
   constructor(
     private auth: AuthService,
     private fb: FormBuilder,
     private router: Router
-  ) {
-    this.form = fb.group({
-      email: [''],
-      password: [''],
-    });
-  }
+  ) {}
 
-  submit() {
-    this.auth.login(this.form.value).subscribe(() => {
+  loginFormSubmit() {
+    this.auth.login(this.loginForm.getRawValue()).subscribe(() => {
       this.router.navigate(['']).catch((err) => console.log(err));
     });
   }
