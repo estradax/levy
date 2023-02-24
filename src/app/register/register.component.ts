ff216649ac0ce6eb9089392a6b4c613a9b8dfb9c
estradax <azharumuhammad@gmail.com>
2023-02-24 21:35:18 +0700

    add register validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,22 +1,42 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../lib/auth/auth.service';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+
+const passwordMatchingValidator: ValidatorFn = (control: AbstractControl) => {
+  const password = control.get('password')!;
+  const passwordConfirmation = control.get('password_confirmation')!;
+  return password.value === passwordConfirmation.value
+    ? null
+    : { notMatch: true };
+};
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  registerForm = this.fb.nonNullable.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    password: ['', [Validators.required]],
-    password_confirmation: ['', [Validators.required]],
-  });
+  registerForm = this.fb.nonNullable.group(
+    {
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      password_confirmation: [''],
+    },
+    {
+      validators: passwordMatchingValidator,
+    }
+  );
   constructor(
     private auth: AuthService,
     private fb: FormBuilder,
