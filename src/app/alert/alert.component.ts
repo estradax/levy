a39d1fc83db02305dcbe1ff71b5fe46c457184a9
estradax <azharumuhammad@gmail.com>
2023-02-26 18:17:22 +0700

    add alert component

---
+++ src/app/alert/alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  standalone: true,
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.css']
+})
+export class AlertComponent {
+
+}
