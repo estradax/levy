c1f79b86480dd8c0025ae6096243592798730efc
estradax <azharumuhammad@gmail.com>
2023-03-15 08:22:42 +0700

    make alert disposeable

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AlertService } from '../lib/alert/alert.service';
 import { AsyncPipe, NgIf } from '@angular/common';
+import {delayWhen, interval} from "rxjs";
 
 @Component({
   selector: 'app-alert',
@@ -9,6 +10,24 @@ import { AsyncPipe, NgIf } from '@angular/common';
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css'],
 })
-export class AlertComponent {
+export class AlertComponent implements OnInit {
+  isOpen$ = this.alertService.isOpen$.pipe(
+    delayWhen((isOpen) => {
+      if (!isOpen) return interval(5000);
+      return interval(0);
+    })
+  );
+
+  isOpen = false;
+
   constructor(public alertService: AlertService) {}
+
+  ngOnInit() {
+    this.isOpen$.subscribe((isOpen) => {
+      this.isOpen = isOpen;
+      if (isOpen) {
+        this.alertService.close();
+      }
+    });
+  }
 }
